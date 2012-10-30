78ebe145ed4bd13f2d61c1290e48051785451a2e
Nicholas Fine <nicholas.fine@gmail.com>
2012-10-30 11:21:23 -0500

    link methods

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,17 @@
 class Link < ActiveRecord::Base
   attr_accessible :original_url, :slug
+
+  validate :original_url, :slug, presence: true
+  validate :slug, unique: true
+
+  after_initialize :handle_slug_creation, unless: :slug?
+
+  def to_param
+    slug
+  end
+
+  def handle_slug_creation
+    self.slug = SecureRandom.urlsafe_base64(5)
+  end
+
 end
