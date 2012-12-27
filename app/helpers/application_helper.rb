7e9dcc14bc92377318786f20370c78dfcdd1634c
Nicholas Fine <nicholas.fine@gmail.com>
2012-12-26 20:53:47 -0600

    Attribution and License
    - All Rites Reversed
    - Attribute Me!
    - License

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def tweetbot_custom_url user
     get_create_link_url + "?auth_token=#{user.authentication_token}&original_url=%@"
   end
+
+  def crisco_version
+    `cat ./VERSION`
+  end
 end
